f47863c58652d0e735fadfc207cf1215c012e3df
RunThem <iccy.fun@outlook.com>
2023-02-19 03:09:23 +0800

    feat(memory.c,.h): encapsulates memory allocation functions.

---
+++ include/memory.h
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) 2023 by RunThem <iccy.fun@outlook.com> - All rights reserved.
+ */
+
+#ifndef _U_MEMORY_H_
+#define _U_MEMORY_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <types.h>
+
+void u_memory_set_malloc(u_nullptr_t (*f_malloc)(size_t));
+void u_memory_set_calloc(u_nullptr_t (*f_calloc)(size_t, size_t));
+void u_memory_set_realloc(u_nullptr_t (*f_realloc)(u_nullptr_t, size_t));
+void u_memory_set_free(void (*f_free)(u_nullptr_t));
+
+u_nullptr_t u_malloc(size_t sz);
+u_nullptr_t u_calloc(size_t cnt, size_t sz);
+u_nullptr_t u_zalloc(size_t sz);
+u_nullptr_t u_realloc(u_nullptr_t ptr, size_t sz);
+void u_free(u_nullptr_t ptr);
+
+#ifdef __cplusplus
+} /* extern "C" */
+#endif
+
+#endif /* !_U_MEMORY_H_ */
